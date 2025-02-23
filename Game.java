a4c7a567dd9d7f703e540d7204abc513eaac35ef
Luke Barkess <luke.barkess@icloud.com>
2025-02-23 20:22:14 +0000

    game

---
+++ Game.java
@@ -0,0 +1,22 @@
+public class Game {
+    public static void main(String[] args) {
+        GameArena arena = new GameArena(1600, 900);
+
+        Player p = new Player(arena, 0, 0, 5, 100);
+        EnemyManager enemMan = new EnemyManager(arena, p);
+        int frames = 0;
+
+        while (true) {
+            if (frames == 100) {
+                enemMan.createEnemy(1920, 1080, 50, 80);
+                frames = 0;
+            }
+            arena.pause();
+
+            p.update();
+            enemMan.moveEnemies();
+            enemMan.checkCollisions();
+            frames++;
+        }
+    }
+}
