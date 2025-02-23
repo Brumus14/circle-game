9738822c381c44cdec9e5842a56732b26a5a1d35
Luke Barkess <luke.barkess@icloud.com>
2025-02-23 20:05:47 +0000

    bullets despawn

--- Game.java
+++ Game.java
@@ -1,14 +1,13 @@
 public class Game {
     public static void main(String[] args) {
-        GameArena arena = new GameArena(1920, 1080);
+        GameArena arena = new GameArena(1600, 900);
 
         Player p = new Player(arena, 0, 0, 5, 100);
         EnemyManager enemMan = new EnemyManager(arena, p);
         int frames = 0;
 
-
         while (true) {
-            if(frames == 100){
+            if (frames == 100) {
                 enemMan.createEnemy(1920, 1080, 50, 80);
                 frames = 0;
             }
