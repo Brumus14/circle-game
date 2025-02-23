1a8029ec6833973fd681e2852701d0f5802995a2
Hypnoticat <spyracat@gmail.com>
2025-02-23 19:42:05 +0000

    everything working together

--- Game.java
+++ Game.java
@@ -3,11 +3,21 @@ public class Game {
         GameArena arena = new GameArena(1920, 1080);
 
         Player p = new Player(arena, 0, 0, 5, 100);
+        EnemyManager enemMan = new EnemyManager(arena, p);
+        int frames = 0;
+
 
         while (true) {
+            if(frames == 100){
+                enemMan.createEnemy(1920, 1080, 50, 80);
+                frames = 0;
+            }
             arena.pause();
 
             p.update();
+            enemMan.moveEnemies();
+            enemMan.checkCollisions();
+            frames++;
         }
     }
 }
