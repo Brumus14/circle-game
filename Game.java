9e13d2dd6bb6f4acf7a2e53488b8ff9be07ebd8c
Luke Barkess <luke.barkess@icloud.com>
2025-02-23 18:58:16 +0000

    small Game change

--- Game.java
+++ Game.java
@@ -1,25 +1,13 @@
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
 public class Game {
     public static void main(String[] args) {
         GameArena arena = new GameArena(1920, 1080);
 
-        JFrame frame =
-            (JFrame)SwingUtilities.getWindowAncestor(arena.getPanel());
-
         Player p = new Player(arena, 0, 0, 5, 100);
-
-        arena.setSize(200, 200);
 
         while (true) {
             arena.pause();
 
             p.update();
-
-            // java.awt.Rectangle bounds = frame.getBounds();
-            // arena.setSize(bounds.width, bounds.height);
-            arena.setSize(1920, 1080);
         }
     }
 }
